f6080b720774fe2a59666225f3b0dfdb578951d2
qotmddnjs <dudcnsaus@gmail.com>
2024-01-26 17:26:42 +0900

    컨트롤액션

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -10,32 +10,20 @@ public class UsrHomeController {
 	private int count;
 
 	public UsrHomeController() {
-		count = 0; 
+		count = 0;
 	}
 
-	@RequestMapping("/usr/home/main4")
+	@RequestMapping("/usr/home/setCount")
 	@ResponseBody
-	public int showMain4() {
+	public String setCount(int count) {
+		this.count = count;
+		return "count의 값이 " + count + "(으)로 초기화 되었습니다.";
+	}
+
+	@RequestMapping("/usr/home/getCount")
+	@ResponseBody
+	public int getCount() {
 		return count++;
 	}
 
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String showMain() {
-		return "안녕하세요";
-	}
-
-	@RequestMapping("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "잘가";
-	}
-
-	@RequestMapping("/usr/home/main3")
-	@ResponseBody
-	public int showMain3() {
-		int a = 1;
-		int b = 2;
-		return a + b;
-	}
 }

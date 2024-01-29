3363e73b049a99ef6ba79b8744a9168da00db825
qotmddnjs <dudcnsaus@gmail.com>
2024-01-29 11:18:50 +0900

    게시물 테스트 데이터 생성, 작성 로직 중복 제거

--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Article {
+	public Article(int i, String string, String string2, int j) {
+		
+	}
 	int id;
 	String title;
 	String body;

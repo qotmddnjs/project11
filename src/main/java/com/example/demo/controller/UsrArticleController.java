bb6aa89e7173933bb64ad2c5b6a7bc7b39398e97
qotmddnjs <dudcnsaus@gmail.com>
2024-01-30 17:49:00 +0900

    doModify,doDelete에 ResultDate 적용 1

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -64,18 +64,17 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public Object doModify(int id, String title, String body) {
+	public ResultData doModify(int id, String title, String body) {
 		System.err.println("title : " + title);
 		System.err.println("body : " + body);
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 글은 존재하지 않습니다.";
-		}
+			return ResultData.from("F-1", Ut.f("%d번 게시물은 존재하지 않습니다", id));		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 수정되었습니다", id));
 	}
 
 	@RequestMapping("/usr/article/doDelete")

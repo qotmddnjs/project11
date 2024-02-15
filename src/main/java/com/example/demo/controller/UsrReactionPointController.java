a58a1fe643faab068d4b13530518043252d4c4ff
qotmddnjs <dudcnsaus@gmail.com>
2024-02-15 14:28:20 +0900

    좋아요 싫어요 버튼 처리 구현중

---
+++ src/main/java/com/example/demo/controller/UsrReactionPointController.java
@@ -0,0 +1,78 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.service.ReactionPointService;
+import com.example.demo.util.Ut;
+import com.example.demo.vo.ResultData;
+import com.example.demo.vo.Rq;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class UsrReactionPointController {
+
+	@Autowired
+	private Rq rq;
+
+	@Autowired
+	private ReactionPointService reactionPointService;
+
+	// 액션 메서드
+
+	@RequestMapping("/usr/reactionPoint/doGoodReaction")
+	@ResponseBody
+	public ResultData doGoodReaction(String relTypeCode, int relId, String replaceUri) {
+
+		ResultData usersReactionRd = reactionPointService.usersReaction(rq.getLoginedMemberId(), relTypeCode, relId);
+
+		int usersReaction = (int) usersReactionRd.getData1();
+
+		if (usersReaction == 1) {
+			ResultData rd = reactionPointService.deleteGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+			return ResultData.from("S-1", "좋아요 취소");
+		} else if (usersReaction == -1) {
+			ResultData rd = reactionPointService.deleteBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+			rd = reactionPointService.addGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+			return ResultData.from("S-2", "싫어요 눌렀잖어");
+		}
+
+		ResultData reactionRd = reactionPointService.addGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+
+		if (reactionRd.isFail()) {
+			return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
+		}
+
+		return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
+	}
+
+	@RequestMapping("/usr/reactionPoint/doBadReaction")
+	@ResponseBody
+	public ResultData doBadReaction(String relTypeCode, int relId, String replaceUri) {
+
+		ResultData usersReactionRd = reactionPointService.usersReaction(rq.getLoginedMemberId(), relTypeCode, relId);
+
+		int usersReaction = (int) usersReactionRd.getData1();
+
+		if (usersReaction == -1) {
+			ResultData rd = reactionPointService.deleteBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+			return ResultData.from("S-1", "싫어요 취소");
+		} else if (usersReaction == 1) {
+			ResultData rd = reactionPointService.deleteGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+			rd = reactionPointService.addBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+			return ResultData.from("S-2", "좋아요 눌렀잖어");
+		}
+
+		ResultData reactionRd = reactionPointService.addBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
+
+		if (reactionRd.isFail()) {
+			return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
+		}
+
+		return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
+	}
+
+}
